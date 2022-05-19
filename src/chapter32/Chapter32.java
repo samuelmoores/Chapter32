083d69a2025221fde235f7db5599b80f47a26cf9
samuelmoores <moores_samuel_dylan@student.smc.edu>
2022-05-18 20:10:03 -0700

    Update

--- src/chapter32/Chapter32.java
+++ src/chapter32/Chapter32.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package chapter32;
 
-/**
- *
- * @author samue
- */
+import java.sql.*;
+
 public class Chapter32 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) 
+            throws SQLException, ClassNotFoundException {
+        
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL");
+
     }
     
 }
